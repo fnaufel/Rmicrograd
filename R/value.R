0b26013c0770e252f2f9a870ad8353a752044ec6
Fernando Naufel <fnaufel@gmail.com>
2023-10-16 11:55:20 -0300

    Error when doing topological sort
    Storing R6 objects in a list and testing for membership is tricky.

--- R/value.R
+++ R/value.R
@@ -1,4 +1,3 @@
-
 Class <- R6::R6Class
 
 
@@ -11,22 +10,52 @@ Value <- Class(
     data = 0,
     prev = list(),
     op = '',
-    backward = NULL,
     grad = 0,
+    # To be assigned depending on op
+    backward_ = NULL,
+    # Performs topological sorting and calls backward_ on self
+    backward = function() {
+      topo <- list()
+      visited <- list()
+
+      # TODO: fix this: topo and visited have only one element
+      build_topo <- function(v) {
+
+        browser()
+
+        # The problem is that the following test always returns FALSE!
+        # I.e., R thinks that v has already been visited, even when it has not!
+        # Is it because I did not implement equality for Value objects?
+        # Too much work. Go to Julia.
+        if (!(list(v) %in% visited)) {
+          visited <<- unique(append(visited, list(v)))
+          for (child in v$prev) {
+            build_topo(child)
+          }
+          topo <<- append(topo, list(v))
+        }
+      }
+
+      build_topo(self)
+      self$grad <- 1
+      for (v in rev(topo)) {
+        v$backward_()
+      }
+    },
 
     # Constructor
     initialize = function(
       data = 0,
       prev = list(),
       op = '',
-      backward = \() NULL
+      backward_ = \() NULL
     ) {
       stopifnot('data must be numeric' = is.numeric(data))
       stopifnot('data must be length 1' = length(data) == 1)
       self$data = data
       self$prev = prev
       self$op = op
-      self$backward = backward
+      self$backward_ = backward_
     },
 
     # ReLU method (calls function that returns new Value object)
@@ -92,7 +121,7 @@ Value <- Class(
     prev = list(v1, v2),
     op = '+'
   )
-  out$backward <- \() {
+  out$backward_ <- \() {
     v1$grad <- v1$grad + out$grad
     v2$grad <- v2$grad + out$grad
   }
@@ -128,7 +157,7 @@ Value <- Class(
     prev = list(v1, v2),
     op = '*'
   )
-  out$backward <- \() {
+  out$backward_ <- \() {
     v1$grad <- v1$grad + v2$data * out$grad
     v2$grad <- v2$grad + v1$data * out$grad
   }
@@ -158,7 +187,7 @@ Value <- Class(
     prev = list(v1),
     op = paste0('^', v2)
   )
-  out$backward <- \() {
+  out$backward_ <- \() {
     v1$grad <- v1$grad + v2 * v1$data^(v2 - 1) * out$grad
   }
   out
@@ -178,7 +207,7 @@ relu = function(v1) {
     prev = list(v1),
     op = 'ReLU'
   )
-  out$backward <- \() {
+  out$backward_ <- \() {
     v1$grad <- v1$grad + ifelse(v1$data <= 0, 0, 1) * out$grad
   }
   out
